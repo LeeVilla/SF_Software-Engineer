61114a2e3def1f75a49e5da14caa3cff2a1f33b7
LeeVilla <110144542+LeeVilla@users.noreply.github.com>
2022-08-28 18:27:09 -0400

    Jasmine exercise

---
+++ jasmine-testing-exercises/calculator/calculator-test.js
@@ -0,0 +1,33 @@
+
+it('should calculate the monthly rate correctly', function () {
+  // ...
+  const values = {
+    amount : 10000,
+    years : 8,
+    rate: 5.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('130.44')
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  // ..
+   const values = {
+    amount: 10043,
+    years: 8,
+    rate: 5.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('131.00');
+});
+
+it("should calculate longer term", function() {
+  // ..
+   const values = {
+    amount: 10000,
+    years: 20,
+    rate: 7
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('77.53');
+});
+
+/// etc
