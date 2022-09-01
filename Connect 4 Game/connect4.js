38480777b9dc81e7e6002a3fcf3bda79dc4a5789
LeeVilla <110144542+LeeVilla@users.noreply.github.com>
2022-09-01 19:27:03 -0400

    Connect 4 (take 1)

---
+++ Connect 4 Game/connect4.js
@@ -0,0 +1,118 @@
+const playerRed = "R";
+const playerBlue = "B";
+let currentPlayer = playerRed;
+
+let gameOver = false;
+var board;
+
+const rows = 6;
+const columns = 7;
+let currentColumns;
+
+window.onload = function () {
+  setGame();
+};
+
+function setGame() {
+  board = [];
+  currentColumns = [5, 5, 5, 5, 5, 5, 5];
+
+  for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < columns; c++) {
+      row.push(" ");
+
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.classList.add("tile");
+      tile.addEventListener("click", setPiece);
+      document.getElementById("board").append(tile);
+    }
+    board.push(row);
+  }
+}
+
+function setPiece() {
+  if (gameOver) {
+    return;
+  }
+
+  let coords = this.id.split("-");
+  let r = parseInt(coords[0]);
+  let c = parseInt(coords[1]);
+
+  r = currentColumns[c];
+  if (r < 0){
+    return; 
+  }
+
+  board[r][c] = currentPlayer;
+  let tile = document.getElementById(r.toString() + "-" + c.toString());
+  if (currentPlayer == playerRed) {
+    tile.classList.add("red");
+    currentPlayer = playerBlue;
+  } else {
+    tile.classList.add("blue");
+    currentPlayer = playerRed;
+  }
+  r -= 1;
+  currentColumns[c] = r; 
+
+  checkWinner ();
+}
+
+function checkWinner () {
+    // Horizontal Check
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns -3; c++) {
+            if(board[r][c] != ' '){
+                if (board [r][c] == board [r][c+1] && board [r][c+1] == board [r][c+2] && board [r][c+2] == board [r][c+3]){
+                setWinner(r,c);
+                return;
+                }
+            }
+        }
+    }
+ 
+// Vertical Check
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows -3; r++) {
+            if(board[r][c] != ' '){
+                if (board [r][c] == board [r+1][c] && board [r+1][c] == board [r+2][c] && board [r+2][c] == board [r+3][c]){
+                setWinner(r,c);
+                return;
+                }
+            }
+        }
+    }
+    // Diagonal Check
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns -3; c++) {
+            if(board[r][c] != ' '){
+                if (board [r][c] == board [r+1][c+1] && board [r+1][c+1] == board [r+2][c+2] && board [r+2][c+2] == board [r+3][c+3]){
+                setWinner(r,c);
+                return;
+                }
+            }
+        }
+    }
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns -3; c++) {
+            if(board[r][c] != ' '){
+                if (board [r][c] == board [r-1][c+1] && board [r-1][c+1] == board [r-2][c+2] && board [r-2][c+2] == board [r-3][c+3]){
+                setWinner(r,c);
+                return;
+                }
+            }
+        }
+    }
+}
+function setWinner (r,c){
+let winner = document.getElementById("winner");
+if (board[r][c] == playerRed){
+    winner.innerText = "Red Wins!";
+} else {
+    winner.innerText = "Blue Wins!";
+}
+gameOver = true;
+}
