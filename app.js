03c4a336289771b21e4cb5cfd7d2aaa9080a28e2
LeeVilla <110144542+LeeVilla@users.noreply.github.com>
2023-01-12 16:59:18 -0500

    Routing and Middleware Exercise

--- app.js
+++ app.js
@@ -1,94 +1,21 @@
 const express = require("express");
 const app = express();
-const ExpressError = require("./exError");
+const itemsRoutes = require("./route/items");
+const ExpressError = require("./expressError");
 
-const {
-  convertAndValidateNumsArray,
-  findMode,
-  findMean,
-  findMedian,
-} = require("./helpers");
-app.get("/mean", function (req, res, next) {
-  if (!req.query.nums) {
-    throw new ExpressError(
-      "takes a query key of nums which is a comma-separated list of numbers",
-      400
-    );
-  }
-  let numsAsStrings = req.query.nums.split(",");
+app.use(express.json());
+app.use("/items", itemsRoutes);
 
-  let nums = convertAndValidateNumsArray(numsAsStrings);
-  if (nums instanceof Error) {
-    throw new ExpressError(nums.message);
-  }
-
-  let result = {
-    operation: "mean",
-    result: findMean(nums),
-  };
-
-  return res.send(result);
+app.use((req, res, next) => {
+  return new ExpressError("Invalid", 404);
 });
 
-app.get("/median", function (req, res, next) {
-  if (!req.query.nums) {
-    throw new ExpressError(
-      "takes a query key of nums which is a comma-separated list of numbers",
-      400
-    );
-  }
-  let numsAsStrings = req.query.nums.split(",");
-
-  let nums = convertAndValidateNumsArray(numsAsStrings);
-  if (nums instanceof Error) {
-    throw new ExpressError(nums.message);
-  }
-
-  let result = {
-    operation: "median",
-    result: findMedian(nums),
-  };
-
-  return res.send(result);
-});
-
-app.get("/mode", function (req, res, next) {
-  if (!req.query.nums) {
-    throw new ExpressError(
-      "takes a query key of nums which is a comma-separated list of numbers",
-      400
-    );
-  }
-  let numsAsStrings = req.query.nums.split(",");
-
-  let nums = convertAndValidateNumsArray(numsAsStrings);
-  if (nums instanceof Error) {
-    throw new ExpressError(nums.message);
-  }
-
-  let result = {
-    operation: "mode",
-    result: findMode(nums),
-  };
-
-  return res.send(result);
-});
-
-app.use(function (req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-
-  return next(err);
-});
-
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
 
   return res.json({
-    error: err,
-    message: err.message,
+    error: err.message,
   });
 });
 
-app.listen(3000, function () {
-  console.log(`Server starting on port 3000`);
-});
+module.exports = app;
