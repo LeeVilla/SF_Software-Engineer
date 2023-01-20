b8b059c6e87fa673aaf2725f44dd62519f8b32bc
LeeVilla <110144542+LeeVilla@users.noreply.github.com>
2023-01-20 07:29:33 -0500

    Jobly

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-/** Express app for Lunchly. */
+"use strict";
+
+/** Express app for jobly. */
 
 const express = require("express");
-const nunjucks = require("nunjucks");
-const bodyParser = require("body-parser");
-const routes = require("./routes");
+const cors = require("cors");
+
+const { NotFoundError } = require("./expressError");
+
+const { authenticateJWT } = require("./middleware/auth");
+const authRoutes = require("./routes/auth");
+const companiesRoutes = require("./routes/companies");
+const usersRoutes = require("./routes/users");
+const jobsRoutes = require("./routes/jobs");
+
+const morgan = require("morgan");
 
 const app = express();
 
-// Parse body for urlencoded (non-JSON) data
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authenticateJWT);
 
-nunjucks.configure("templates", {
-  autoescape: true,
-  express: app
+app.use("/auth", authRoutes);
+app.use("/companies", companiesRoutes);
+app.use("/users", usersRoutes);
+app.use("/jobs", jobsRoutes);
+
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
 });
 
-app.use(routes);
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
 
-/** 404 handler */
-
-app.use(function(req, res, next) {
-  const err = new Error("Not Found");
-  err.status = 404;
-
-  // pass the error to the next piece of middleware
-  return next(err);
-});
-
-/** general error handler */
-
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-
-  return res.render("error.html", { err });
+  return res.status(status).json({
+    error: { message, status },
+  });
 });
 
 module.exports = app;
