d7f1568306a3013aa74e27a80ed35c799134413c
LeeVilla <110144542+LeeVilla@users.noreply.github.com>
2023-01-22 22:03:05 -0500

    react-lights-out

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
+import React from "react";
+import Board from "./Board";
 import "./App.css";
-import Carousel from "./Carousel";
-import photos from "./photos.js";
 
-/** App component, renders Carousel
- * 
- * Props: none
- * State: none
- * 
- * App --> Carousel
- */
+/** Simple app that just shows the LightsOut game. */
 
 function App() {
-
-  // In real life, this data would probably come from an AJAX call.
-  // For our purposes, we're just importing from another file.
-  const carouselPhotos = photos;
-  const carouselTitle = "Shells from far-away beaches";
-
   return (
-    <div className="App">
-      <Carousel photos={carouselPhotos} title={carouselTitle} />
-    </div>
+      <div className="App">
+        <Board />
+      </div>
   );
 }
 
