2e7e07572a0a2cbe6e202e6622215c83a03a1d9d
LeeVilla <110144542+LeeVilla@users.noreply.github.com>
2023-01-22 21:37:40 -0500

    React Testing Exercises»

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
-import Board from "./Board";
 import "./App.css";
+import Carousel from "./Carousel";
+import photos from "./photos.js";
 
-/** Simple app that just shows the LightsOut game. */
+/** App component, renders Carousel
+ * 
+ * Props: none
+ * State: none
+ * 
+ * App --> Carousel
+ */
 
 function App() {
+
+  // In real life, this data would probably come from an AJAX call.
+  // For our purposes, we're just importing from another file.
+  const carouselPhotos = photos;
+  const carouselTitle = "Shells from far-away beaches";
+
   return (
-      <div className="App">
-        <Board />
-      </div>
+    <div className="App">
+      <Carousel photos={carouselPhotos} title={carouselTitle} />
+    </div>
   );
 }
 
